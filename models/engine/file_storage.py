48f2760a403fdf5082ce94f493106ac45a5e42d5
Rana Ebadah <ranaebadah@hotmail.com>
2024-03-17 04:17:12 +0400

    task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import shlex
 
 
 class FileStorage:
@@ -8,9 +9,19 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls == None:
+            return FileStorage.__objects
+        else:
+            newObjects = {}
+            for  key in self.__objects:
+                newKey = key.replace('.', ' ')
+                newKey = shlex.split(newKey)
+                if (newKey[0] == cls.__name__):
+                    newObjects[key] = self.__objects[key]
+            return (newObjects)
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +59,12 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it is inside"""
+        if obj == None:
+            return
+        else:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del FileStorage.__objects[key]
+            FileStorage.save(self)
